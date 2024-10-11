470dc22597c8422d7a2537b22aac9bfff83a8871
Tim3088 <302023315386@zjut.edu.cn>
2024-10-11 16:01:30 +0800

    feat(controller):开发了获取功能列表接口

--- config/router/router.go
+++ config/router/router.go
@@ -13,6 +13,7 @@ func Init(r *gin.Engine) {
 	{
 		userRouterInit(api)
 		funcRouterInit(api)
+		systemRouterInit(api)
 	}
 
 }
--- config/router/systemRouter.go
+++ config/router/systemRouter.go
@@ -1,1 +1,11 @@
 package router
+
+import (
+	"wejh-go/app/controllers/systemController"
+
+	"github.com/gin-gonic/gin"
+)
+
+func systemRouterInit(r *gin.RouterGroup) {
+	r.POST("/applist", systemController.GetAppList)
+}
